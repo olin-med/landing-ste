7a7e6407baf4ccd74fe1f14f1d6c4b785fb2f203
olin-med <olin.costa@sou.inteli.edu.br>
2024-04-17 23:01:26 -0300

    adds embed video and checkout page

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+import React, { useEffect } from 'react';
+
+const HotmartCheckout: React.FC = () => {
+  useEffect(() => {
+    // Function to dynamically load the Hotmart script
+    const loadScript = () => {
+      const script = document.createElement('script');
+      script.src = "https://checkout.hotmart.com/lib/hotmart-checkout-elements.js";
+      script.async = true;
+      script.onload = () => initCheckout();
+      document.body.appendChild(script);
+    };
+
+    // Initialize the Hotmart checkout
+    const initCheckout = () => {
+      // Ensure the checkoutElements object is available in the global scope
+      const checkoutElements = (window as any).checkoutElements;
+      
+      if (checkoutElements) {
+        const elements = checkoutElements.init('inlineCheckout', {
+          offer: 'kjl7fk5t' // This should be your offer code
+        });
+        elements.mount('#inline_checkout');
+      }
+    };
+
+    // Load the script when the component mounts
+    loadScript();
+
+    // Optional: Cleanup function to run when the component unmounts
+    return () => {
+      // Perform cleanup if necessary (like unmounting the checkout)
+    };
+  }, []); // Empty dependency array means this effect will only run once, after the initial render
+
+  return (
+    <div>
+      <div className="relative w-full bg-black overflow-hidden image-background">
+        <img 
+          src="./background.jpg" 
+          alt="Background" 
+          className="w-full object-cover object-center" 
+          style={{ height: 'calc(100vh)', objectPosition: '50% 35%', filter: 'blur(6px)' }}
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-center min-h-screen">
+          <div id="inline_checkout">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HotmartCheckout;
--- src/app/ebook/page.tsx
+++ src/app/ebook/page.tsx
@@ -3,13 +3,45 @@ import type { NextPage } from 'next';
 import { useRouter } from 'next/navigation';
 
 const Ebook: NextPage = () => {
-    const router = useRouter();
-    return (
-      <div>
-        <h1>EBOOK</h1>
-        <p>This is the content of card 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  const router = useRouter();
+
+  // Example YouTube video ID
+  const videoId = "YOUR_YOUTUBE_VIDEO_ID"; // Replace this with your video ID
+
+  // YouTube embed URL construction
+  const embedUrl = `https://youtu.be/dQw4w9WgXcQ?feature=shared`;
+
+  return (
+    <div>
+      <div className="relative w-full bg-black overflow-hidden image-background">
+        <img 
+          src="./background.jpg" 
+          alt="Background" 
+          className="w-full object-cover object-center" 
+          style={{ height: 'calc(100vh)', objectPosition: '50% 35%', filter: 'blur(6px)' }}
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-center min-h-screen">
+          <h1 className="text-4xl sm:text-8xl text-center text-white playfair cursor-default">
+            Ebook
+          </h1>
+          <p className="text-sm sm:text-lg text-center text-white cursor-default p-4 sm:p-8">
+            O seu guia completo para a Criação de Conteúdo Digital!
+          </p>
+          {/* Video Embed */}
+          <div className="w-full max-w-4xl p-4">
+            <iframe
+              className="w-full aspect-video"
+              src='https://www.youtube.com/embed/dQw4w9WgXcQ?si=vBkTCV06cuO2WZ0R'
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
       </div>
-    );
-  };
-  
-  export default Ebook;
+    </div>
+  );
+};
+
+export default Ebook;
